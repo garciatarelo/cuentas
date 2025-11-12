164b7a24c67d2d57fdd44757675538eefb45180a
Pamela Tarelo <pamelatarelo18@gmail.com>
2025-11-12 16:13:24 -0600

    React

--- cuentas-front/src/App.jsx
+++ cuentas-front/src/App.jsx
@@ -2,32 +2,34 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import ItemComponent from './components/itemComponent'
+import Button from 'react-bootstrap/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  
+  var x = 0
+
+  //React Mooks - Ganchos de React
+  let[contador, setContador] = useState(10)
+  let[arr, setArr] = useState([])
+
+  let clickButton=()=>{
+    setContador(++contador)
+    console.log(contador)
+    let x = [...arr, contador]
+    setArr(x)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Contador</h1>
+      <p>{contador}</p>
+      <Button variant="primary">Primary</Button>
+      
+      <button onClick={clickButton}>Aumentar</button>
+      {arr.map((item) => (
+        <ItemComponent key={item} />
+      ))}
     </>
   )
 }
---
+++ cuentas-front/src/components/ItemComponent.jsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react"
+
+export default function ItemComponent() {
+let [valor, setValor] = useState(1)
+    return (
+    <>
+        <h2>Item{valor}</h2>
+        <input type="number" onChange={(e)=>{
+            setValor(e.target.value)}} />
+    </>
+    )
+
+}
